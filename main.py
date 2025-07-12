7491d61a37148cb6bfaee50627e0ead3c4f67c69
Seb GG <sebastien.giguet@ynov.com>
2025-07-12 16:13:44 +0200

    fix: clean pyc and stable build for pyinstaller

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ root.title("Gestion des ressources GCP")
 root.geometry("500x450")
 root.resizable(False, False)
 
-# 🌟 Logo affiché en haut de l'application
+# 🌟 Logo affiché en haut de l'appli
 try:
     logo_path = resource_path("F1stream_logo.png")
     logo_image = Image.open(logo_path)
